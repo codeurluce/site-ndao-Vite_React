f496eb8a390eae82efc8a0b6b4cfafd1d051d3ae
Luce <boama561@gmail.com>
2025-05-23 12:47:52 +0000

    Responsive de section pretres

--- src/components/home/PriestSchedule.jsx
+++ src/components/home/PriestSchedule.jsx
@@ -57,7 +57,7 @@ export function PriestSchedule() {
           </h2>
         </div>
 
-        <div className="grid md:grid-cols-4 gap-2">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
           {priestSchedule.map((priest) => (
 
             <div
