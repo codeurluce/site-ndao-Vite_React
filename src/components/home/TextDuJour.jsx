a8c7b136e16f289d26c99c126b2c24a1f6f4377c
Luce <boama561@gmail.com>
2025-05-28 01:59:42 +0000

    Gestion de textLiturgiques

--- src/components/home/TextDuJour.jsx
+++ src/components/home/TextDuJour.jsx
@@ -16,7 +16,7 @@ const capitalizeFirstLetter = (str) => str.charAt(0).toUpperCase() + str.slice(1
 // Fonction pour obtenir le jour liturgique pour une date donnée
 function getLiturgicalDayForDate(dateStr, calendar) {
   const found = calendar.jour.find(entry => entry.date === dateStr);
-  return found ? found.fete : "Pas de fête liturgique aujourd'hui";
+  return found ? found.fete : "Pas de fere";
 }
 
 
@@ -33,20 +33,17 @@ export const DailyReadings = () => {
       {
         title: "Première lecture",
         reference: "Daniel 3, 14-20.91-92.95",
-        fullText:
-          'En ces jours-là, le roi Nabucodonosor dit : « Est-il vrai, Shadrak, Méshak et Abed-Nego, que vous ne servez pas mes dieux et que vous n\'adorez pas la statue d\'or que j\'ai fait ériger ? » de la cithare, de la harpe, de la lyre, de la cornemuse et de tous les genres d\'instruments, si vous êtes prêts à vous prosterner pour adorer la statue que j\'ai faite... Mais si vous ne l\'adorez pas, vous serez immédiatement jetés dans la fournaise de feu ardent ; et quel est le dieu qui vous délivrera de ma main ?"',
+        fullText:"En ces jours-là, dans la ville de Philippes, la foule se déchaîna contre Paul et Silas. Les magistrats ordonnèrent de leur arracher les vêtements pour leur donner la bastonnade. Après les avoir roués de coups, on les jeta en prison, en donnant au geôlier la consigne de les surveiller de près. Pour appliquer cette consigne, il les mit tout au fond de la prison, avec les pieds coincés dans des blocs de bois. Vers le milieu de la nuit, Paul et Silas priaient et chantaient les louanges de Dieu, et les autres détenus les écoutaient. Tout à coup, il y eut un violent tremblement de terre, qui secoua les fondations de la prison : à l’instant même, toutes les portes s’ouvrirent, et les liens de tous les détenus se détachèrent. Le geôlier, tiré de son sommeil, vit que les portes de la prison étaient ouvertes ; croyant que les détenus s’étaient évadés, il dégaina son épée et il était sur le point de se donner la mort. Mais Paul se mit à crier d’une voix forte : « Ne va pas te faire de mal, nous sommes tous là. » Ayant réclamé de la lumière, le geôlier se précipita et, tout tremblant, se jeta aux pieds de Paul et de Silas. Puis il les emmena dehors et leur demanda : « Que dois-je faire pour être sauvé, mes seigneurs ? » Ils lui répondirent : « Crois au Seigneur Jésus, et tu seras sauvé, toi et toute ta maison. » Ils lui annoncèrent la parole du Seigneur, ainsi qu’à tous ceux qui vivaient dans sa maison. À l’heure même, en pleine nuit, le geôlier les emmena pour laver leurs plaies. Aussitôt, il reçut le baptême avec tous les siens. Puis il fit monter chez lui Paul et Silas, il fit préparer la table et, avec toute sa maison, il laissa déborder sa joie de croire en Dieu. – Parole du Seigneur.",
       },
       {
         title: "Psaume",
         reference: "Daniel 3, 52-56",
-        fullText:
-          "Béni sois-tu, Seigneur, Dieu de nos pères : R/\nBéni soit le nom très saint de ta gloire : R/\nBéni sois-tu dans ton saint temple de gloire : R/\nBéni sois-tu sur le trône de ton règne : R/\nBéni sois-tu, toi qui sondes les abîmes : R/\nToi qui sièges au-dessus des Kéroubim : R/\nBéni sois-tu au firmament, dans le ciel : R/",
+        fullText:"De tout mon cœur, Seigneur, je te rends grâce : tu as entendu les paroles de ma bouche. Je te chante en présence des anges, vers ton temple sacré, je me prosterne. Je rends grâce à ton nom pour ton amour et ta vérité, car tu élèves, au-dessus de tout, ton nom et ta parole. Le jour où tu répondis à mon appel, tu fis grandir en mon âme la force. Ta droite me rend vainqueur. Le Seigneur fait tout pour moi ! Seigneur, éternel est ton amour : n’arrête pas l’œuvre de tes mains.",
       },
       {
         title: "Évangile",
         reference: "Jean 8, 31-42",
-        fullText:
-          "En ce temps-là, Jésus disait aux Juifs qui croyaient en lui : « Si vous demeurez dans ma parole, vous êtes vraiment mes disciples. » Alors vous connaîtrez la vérité, et la vérité vous rendra libres.\" Ils lui répliquèrent : \"Nous sommes la descendance d'Abraham, et nous n'avons jamais été les esclaves de personne. Comment peux-tu dire : 'Vous deviendrez libres' ?\" Jésus leur répondit : \"Amen, amen, je vous le dis : qui commet le péché est esclave du péché. L'esclave ne demeure pas pour toujours dans la maison ; le fils, lui, y demeure pour toujours.\"",
+        fullText: "En ce temps-là, Jésus disait à ses disciples : « Je m’en vais maintenant auprès de Celui qui m’a envoyé, et aucun de vous ne me demande : “Où vas-tu ?” Mais, parce que je vous dis cela, la tristesse remplit votre cœur. Pourtant, je vous dis la vérité : il vaut mieux pour vous que je m’en aille, car, si je ne m’en vais pas, le Défenseur ne viendra pas à vous ; mais si je pars, je vous l’enverrai. Quand il viendra, il établira la culpabilité du monde en matière de péché, de justice et de jugement. En matière de péché, puisqu’on ne croit pas en moi. En matière de justice, puisque je m’en vais auprès du Père, et que vous ne me verrez plus. En matière de jugement, puisque déjà le prince de ce monde est jugé. » – Acclamons la Parole de Dieu.",
       },
     ],
   };
